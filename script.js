60d1d53f5cf8c21bc9710798dbbc9c5e82f5efc8
abilnf <slin.lbeog@gmail.com>
2022-01-09 11:03:36 +0100

    Change function-classes to real classes

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+  }
 
-  this.info = function () {
+  info() {
     return `${this.title} was written by ${this.author}, ${this.pages} pages, ${this.read ? 'already read' : 'not read yet'}`
   }
 }
